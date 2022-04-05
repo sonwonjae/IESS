722ac96edc87e7664d0ddf02507c485a368664c6
sonwonjae <sonwj091552@gmail.com>
2022-04-05 09:38:00 +0900

    config: project default env configuration - #4
    - next.js
    - firebase
    - styled-components

--- firebase.js
+++ firebase.js
@@ -1,8 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,4 +15,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-export { app, auth, createUserWithEmailAndPassword };
+export { app, auth };
